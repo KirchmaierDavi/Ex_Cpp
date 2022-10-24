94665daa6f53c206c84c2f49d226fb2f9159345e
Davi Kirchmaier Paiva <104040768+KirchmaierDavi@users.noreply.github.com>
2022-10-24 10:00:46 -0300

    inicia resolução de exercicios

--- matriz/matriz.cpp
+++ matriz/matriz.cpp
@@ -3,3 +3,49 @@
 
 using namespace std;
 
+MatrizLin::MatrizLin(int mm, int nn){
+    nl = mm;
+    nc = nn;
+    vet = new float[nl*nc];
+}
+
+MatrizLin::~MatrizLin(){
+    delete [] vet;
+}
+
+int MatrizLin::detInd(int i, int j){
+    if(i >= 0 && i < nl && j >= 0 && j < nc){
+         return i*nc + j;
+    }else{
+        return -1; // indice invalido
+    }
+};
+
+float MatrizLin::get(int i, int j){
+    int k = detInd(i, j);
+    if(k != -1)
+        return vet[k];
+    else {
+        cout << "Indice invalido!" << endl;
+        exit(1);
+    }
+}
+
+void MatrizLin::set(int i, int j, float val){
+    int k = detInd(i, j);
+    if(k != -1)
+        vet[k] = val;
+    else
+        cout << "Indice invalido!" << endl;
+}
+
+void MatrizLin::printaMatriz(){
+    for(int i = 0; i < (nc * nl); i++){
+        if(i == (nc - 1)){
+            cout<< vet[i];
+            cout<< endl;
+        }else{
+            cout<< vet[i] << ", ";
+        }
+    }
+}
--- matriz/matriz.h
+++ matriz/matriz.h
@@ -1,1 +1,18 @@
 #include <iostream>
+
+class MatrizLin
+{
+public:
+    MatrizLin(int mm, int nn);
+    ~MatrizLin();
+    
+    void set(int i, int j, float val);
+    float get(int i, int j);
+    void printaMatriz();
+
+private:
+    int nl, nc; // numero de linhas e colunas
+    float *vet; // vetor de tamanho nl*nc
+    
+    int detInd(int i, int j);
+};
