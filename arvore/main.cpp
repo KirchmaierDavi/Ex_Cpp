b86f56d39af05898b5a5a1b7f65d3d0b2b1d17e7
Davi Kirchmaier Paiva <104040768+KirchmaierDavi@users.noreply.github.com>
2023-01-08 09:44:33 -0300

    resolução do exercicio 1 da lista

---
+++ arvore/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "ArvBin.h"
+using namespace std;
+
+int main()
+{
+    ArvBin arv;
+    arv.montaArvore();
+    arv.insere(15);
+    arv.insere(25);
+    arv.insere(10);
+    arv.preOrdem();
+    cout << "O Numero de Nos e:" << arv.contaNos() << endl;
+    return 0;
+}
